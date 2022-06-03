5975b6a28bc9db2ebc99eacac1663d0ccf332384
hdtopku <1450948930@qq.com>
2022-06-03 23:58:27 +0800

    自动描述：2022-06-03-23:58:27

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   plugins: {
     autoprefixer: {},
+    postcssPlugin: 'internal:charset-removal',
+    AtRule: {
+      charset: (atRule) => {
+        if (atRule.name === 'charset') {
+          atRule.remove();
+        }
+      },
+    },
   },
 };
