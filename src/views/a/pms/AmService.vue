a07fc397f892816d300dabe7cab3f4eca2712503
hdtopku <1450948930@qq.com>
2022-06-07 13:26:59 +0800

    自动描述：2022-06-07-13:26:59

--- src/views/a/pms/AmService.vue
+++ src/views/a/pms/AmService.vue
@@ -1,9 +1,35 @@
 <template>
   <a-card>
     <a-tabs v-model:activeKey="activeKey">
-      <a-tab-pane key="1" tab="欢迎语">
+      <a-tab-pane key="4" tab="服务">
+        <a-divider><a-typography-title :level="2">关于退款</a-typography-title></a-divider>
+        <a-typography-paragraph>
+          <blockquote>
+            凡是申请退款，<a-typography-text mark>一律无理由同意退款</a-typography-text>，无需与买家理论，有些买家就是想白嫖，不给退，就千方百计给差评。
+            <div> 1、一个差评可能会导致后面下单顾客的犹豫，还会使店铺评分从4.9高，降低到4.9低。</div>
+            <div> 2、淘宝不去评判买家对错，差评多了，会导致店铺被淘宝风控，好不容易起来的商品，可能被淘宝无情下架。</div>
+            <div> 3、收到差评，还要花0.5-1小时到电脑端写解释（并且解释只能文字不能带图），极其影响情绪，得不偿失。蝇头小利，斤斤计较将难以做大做强。心态放宽，格局拉满，用更多的精力服务优质顾客。</div>
+            <div> 从长远来看，1单差评带来的损失，远大于这一单的价格。消费者走了，差评永远留在了店里，多一事不如少一事，能退都退。以下是负面例子</div>
+          </blockquote></a-typography-paragraph
+        >
+        <a-typography-paragraph mark>
+          如果有想给差评的买家，就跟买家道歉，尽可能引导买家退款：
+          <div>
+            <a-typography-text copyable>十分抱歉，没有给亲带来良好的体验，作为补偿，亲申请退款这边给亲退了，亲看可以吗？希望亲不要因此影响一天美丽的心情呢</a-typography-text>
+          </div>
+        </a-typography-paragraph>
+        <img class="max-h-180 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/678b7d961532ca7c22fe11f68cf99af22d3c8748.jpg" />
+        <a-typography-paragraph mark> 商家拒绝退款，消费者千方百计给差评 </a-typography-paragraph>
+        <img class="max-h-180 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/f5fad2a20599d475b8801a9a37c2c5e7d8ebcc25.jpg" />
+        <a-typography-paragraph mark> 消费者感觉商家说话态度冷漠（可能有时候会感觉身心俱疲，但能赚到钱再辛苦也是值得的，所以尽可能做好服务） </a-typography-paragraph>
+        <img class="max-h-180 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/5189f45062cd777c517d7244b37f24f7f5542300.jpg" />
+        <a-divider><a-typography-title :level="5" mark>以下是正面例子</a-typography-title></a-divider>
+        <a-typography-paragraph mark> 即使价格高，但更能赢得消费者信赖，因为消费者更希望得到尊重 </a-typography-paragraph>
+        <img class="max-h-180 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/f5458c3e1f1b44e4a67a4cd843fc0da5601d18ca.jpg" />
+      </a-tab-pane>
+      <a-tab-pane key="1" tab="欢迎">
         <div class="flex flex-wrap justify-center items-center">
-          <img class="max-w-60 rounded-lg" src="https://article.biliimg.com/bfs/article/750f47328fe6d0f7fab371d6ffe8072f14a2dbb4.jpg" />
+          <img class="max-w-80 rounded-lg" src="https://article.biliimg.com/bfs/article/750f47328fe6d0f7fab371d6ffe8072f14a2dbb4.jpg" />
         </div>
         <a-typography-paragraph>
           <blockquote>
@@ -34,9 +60,9 @@
           3、安卓勿拍，仅iphone、ipad可验证
         </a-typography-paragraph>
         <a-divider>配置方法</a-divider>
-        <div class="flex flex-wrap justify-center items-center">
-          <img class="max-h-60 rounded-lg" src="https://article.biliimg.com/bfs/article/bdeecd46eec80459462ba0afe9558a8dbbf63591.jpg" />
-          <img class="max-h-60 rounded-lg" src="https://article.biliimg.com/bfs/article/e926a12c196e559c83f037dcac2cf276ff374581.jpg" />
+        <div>
+          <img class="max-h-100 rounded-lg" src="https://article.biliimg.com/bfs/article/bdeecd46eec80459462ba0afe9558a8dbbf63591.jpg" />
+          <img class="max-h-100 rounded-lg" src="https://article.biliimg.com/bfs/article/e926a12c196e559c83f037dcac2cf276ff374581.jpg" />
         </div>
       </a-tab-pane>
       <a-tab-pane key="2" tab="问答">
@@ -83,7 +109,7 @@
               </a-typography-paragraph>
             </a-typography-paragraph>
             <div class="flex flex-wrap justify-center items-center">
-              <img class="max-h-80 rounded-lg" src="https://article.biliimg.com/bfs/article/7b874bde1ce69b4096656e2668f6d348fd06f3aa.png" />
+              <img class="max-h-90 rounded-lg" src="https://article.biliimg.com/bfs/article/7b874bde1ce69b4096656e2668f6d348fd06f3aa.png" />
             </div>
           </a-collapse-panel>
           <a-collapse-panel key="5" header="学生优惠？">
@@ -113,19 +139,19 @@
       </a-tab-pane>
       <a-tab-pane key="3" tab="流程">
         <a-divider><a-typography-title :level="2">下单前</a-typography-title></a-divider>
-        <img class="max-h-90 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/162aa5cc81ab4199711aa4cbd23ab8f15305c62f.jpg" />
-        <img class="max-h-90 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/bb6e96e8f40da692213cb845d0ebe689075e8ebc.jpg" />
+        <img class="max-h-150 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/162aa5cc81ab4199711aa4cbd23ab8f15305c62f.jpg" />
+        <img class="max-h-150 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/bb6e96e8f40da692213cb845d0ebe689075e8ebc.jpg" />
         <a-divider><a-typography-title :level="2">下单后</a-typography-title></a-divider>
         <a-divider><a-typography-title :level="5" mark>买家点开链接，步骤一、情况1</a-typography-title></a-divider>
-        <img class="max-h-90 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/e9eefd7f578b1036cd77c9197a13fa26bcdced30.png" />
+        <img class="max-h-150 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/e9eefd7f578b1036cd77c9197a13fa26bcdced30.png" />
         <a-divider><a-typography-title :level="5" mark>步骤一、情况2</a-typography-title></a-divider>
-        <img class="max-h-90 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/6e009cfdacad5abcaff66033fa22a1b36093d4de.png" />
+        <img class="max-h-150 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/6e009cfdacad5abcaff66033fa22a1b36093d4de.png" />
         <a-divider><a-typography-title :level="5" mark>点「已订阅，下一步」，弹窗</a-typography-title></a-divider>
-        <img class="max-h-90 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/00c17d10b55fc1ef30874f4e7bc5633cce56d9f5.png" />
+        <img class="max-h-150 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/00c17d10b55fc1ef30874f4e7bc5633cce56d9f5.png" />
         <a-divider><a-typography-title :level="5" mark>点「继续！提醒消失即成功」，进入步骤二</a-typography-title></a-divider>
-        <img class="max-h-90 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/a4bfabf9888796cd8e300b6afcf8bfb32973753e.png" />
+        <img class="max-h-150 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/a4bfabf9888796cd8e300b6afcf8bfb32973753e.png" />
         <a-divider><a-typography-title :level="5" mark>点「开始验证」，弹窗</a-typography-title></a-divider>
-        <img class="max-h-90 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/60b07e7b899224274c001d8c277b050456212b8d.png" />
+        <img class="max-h-150 rounded-lg mx-auto" src="https://article.biliimg.com/bfs/article/60b07e7b899224274c001d8c277b050456212b8d.png" />
       </a-tab-pane>
     </a-tabs>
   </a-card>
