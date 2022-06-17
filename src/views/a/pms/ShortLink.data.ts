e76bafc5f185754244ba107f5b92712bbb75c45e
hdtopku <1450948930@qq.com>
2022-06-17 11:52:59 +0800

    自动描述：2022-06-17-11:52:59

--- src/views/a/pms/ShortLink.data.ts
+++ src/views/a/pms/ShortLink.data.ts
@@ -5,7 +5,7 @@ import { render } from '/@/utils/common/renderUtils';
 
 export const columns: BasicColumn[] = [
     {
-    title: '账号',
+    title: '短链接码',
     dataIndex: 'code'
    },
    {
@@ -15,6 +15,18 @@ export const columns: BasicColumn[] = [
    {
     title: '备注',
     dataIndex: 'remark'
+   },
+   {
+    title: '访问次数',
+    dataIndex: 'visitCount'
+   },
+   {
+    title: '手机是否可以访问(0-可以,-1-不可以)',
+    dataIndex: 'mobile'
+   },
+   {
+    title: '电脑是否可以访问(0-可以,-1-不可以)',
+    dataIndex: 'pc'
    },
    {
     title: '状态(0：正常，-1:失效)',
@@ -28,7 +40,7 @@ export const columns: BasicColumn[] = [
 
 export const searchFormSchema: FormSchema[] = [
  {
-    label: '账号',
+    label: '短链接码',
     field: 'code',
     component: 'Input'
   },
@@ -43,7 +55,7 @@ export const formSchema: FormSchema[] = [
   // TODO 主键隐藏字段，目前写死为ID
   {label: '', field: 'id', component: 'Input', show: false},
   {
-    label: '账号',
+    label: '短链接码',
     field: 'code',
     component: 'Input',
   },
@@ -56,6 +68,21 @@ export const formSchema: FormSchema[] = [
     label: '备注',
     field: 'remark',
     component: 'Input',
+  },
+  {
+    label: '访问次数',
+    field: 'visitCount',
+    component: 'InputNumber',
+  },
+  {
+    label: '手机是否可以访问(0-可以,-1-不可以)',
+    field: 'mobile',
+    component: 'InputNumber',
+  },
+  {
+    label: '电脑是否可以访问(0-可以,-1-不可以)',
+    field: 'pc',
+    component: 'InputNumber',
   },
   {
     label: '状态(0：正常，-1:失效)',
