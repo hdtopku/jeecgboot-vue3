18b6cc606a6c64e92f7aa1181a3a07b6f5f9666a
hdtopku <1450948930@qq.com>
2022-07-12 09:31:15 +0800

    自动描述：2022-07-12-09:31:15

--- src/views/a/pjlh/ProductList.vue
+++ src/views/a/pjlh/ProductList.vue
@@ -16,7 +16,7 @@
       </a-col>
       <a-col :span="20">
         <div class="pl-2">
-          <div class="mb-3" style="position: fixed">
+          <div class="mb-3">
             <a-input v-model:value="keyword" placeholder="输入商品名称模糊查询" allowClear>
               <template #suffix>
                 <a-button @click="queryList" type="primary">查询</a-button>
