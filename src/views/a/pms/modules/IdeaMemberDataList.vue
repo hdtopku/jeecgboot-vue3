37934c8f717933753d0d807100595530825bdd15
hdtopku <1450948930@qq.com>
2022-10-12 23:27:44 +0800

    自动描述：2022-10-12-23:27:43

--- src/views/a/pms/modules/IdeaMemberDataList.vue
+++ src/views/a/pms/modules/IdeaMemberDataList.vue
@@ -6,24 +6,37 @@
       <a-typography-text v-show="activeKey === '2'" mark>有效+失效的用户</a-typography-text>
     </template>
     <template #top="{ item }">
-      <a-tag> <a-typography-text>标识</a-typography-text> </a-tag>
-      <span v-if="item?.identity.length > 0">
-        <a-typography-text :copyable="advanced"> {{ item.identity }}</a-typography-text>
-      </span>
-
-      <a-tag v-else color="error">未绑定</a-tag>
+      <div> {{ getLocation(item) }}</div>
+      <div> {{ getDevice(item) }}</div>
+      <div>
+        <a-tag>
+          <a-typography-text>标识</a-typography-text>
+        </a-tag>
+        <span v-if="item?.identity.length > 0">
+          <a-typography-text :copyable="advanced"> {{ item.identity }}</a-typography-text>
+        </span>
+        <a-tag v-else color="error">未绑定</a-tag>
+      </div>
     </template>
     <template #bottom="{ item }">
-      <div
-        ><a-tag>打开</a-tag>
+      <div>
+        <a-tag>打开</a-tag>
         <span v-if="item?.openTime?.length > 0">{{ item?.openTime }}</span>
         <a-tag v-else color="error">未打开</a-tag>
       </div>
-      <div><a-tag>激活</a-tag>{{ item?.activeTime }}</div>
-      <!--      <div><a-tag>截止</a-tag>{{ item?.invalidTime }}</div>-->
-      <div><a-tag>截止</a-tag>{{ item?.invalidTime }}</div>
       <div>
-        <a-tag> <a-typography-text>账密</a-typography-text> </a-tag>
+        <a-tag>激活</a-tag>
+        {{ item?.activeTime }}
+      </div>
+      <!--      <div><a-tag>截止</a-tag>{{ item?.invalidTime }}</div>-->
+      <div>
+        <a-tag>截止</a-tag>
+        {{ item?.invalidTime }}
+      </div>
+      <div>
+        <a-tag>
+          <a-typography-text>账密</a-typography-text>
+        </a-tag>
         <span v-if="item?.status === 2 && item?.ideaStatus != null"
           ><a-typography-text
             :delete="item?.ideaStatus === -1"
@@ -52,8 +65,8 @@
         <div
           ><a href="javascript:;" @click="showSysIpInfo(item, true)">{{ item?.ideaUsages.length }}个账号</a></div
         >
-        总{{ item?.count }}次提取</div
-      >
+        总{{ item?.count }}次提取
+      </div>
     </template>
     <template #operate="{ item, index }">
       <a-menu>
@@ -61,8 +74,8 @@
           <a-button type="link" size="small" @click="handleEdit(item)">编辑</a-button>
         </a-menu-item>
         <a-menu-item>
-          <a-button v-if="item.valid === 0" @click="changeValid(item, -1)" type="link" size="small" danger>失效</a-button>
-          <a-button v-if="item.valid === -1" @click="changeValid(item, 0)" type="link" size="small">恢复</a-button>
+          <a-button v-if="item.valid === 0" @click="changeValid(item, -1)" type="link" size="small" danger>失效 </a-button>
+          <a-button v-if="item.valid === -1" @click="changeValid(item, 0)" type="link" size="small"> 恢复 </a-button>
         </a-menu-item>
       </a-menu>
     </template>
@@ -75,7 +88,7 @@
   <a-drawer placement="bottom" closable :visible="showDrawer" @close="onClose">
     <template #title>
       <div>
-        <a-tag>总次数：{{ ipIdeaItem?.showIp ? ipIdeaItem.totalUseCount : ipIdeaItem.totalVisitCount }}</a-tag>
+        <a-tag>总次数：{{ ipIdeaItem?.showIp ? ipIdeaItem.totalUseCount : ipIdeaItem.totalVisitCount }} </a-tag>
         <a-typography-text :copyable="{ text: copyLink(ipIdeaItem?.code) }">
           <span class="" :class="ipIdeaItem?.valid === -1 ? 'text-gray-500 line-through' : 'text-purple-900 font-medium'">
             {{ ipIdeaItem?.code }}</span
@@ -83,7 +96,7 @@
         </a-typography-text>
       </div>
       <div>
-        <a-tag>总条数：{{ ipIdeaItem?.showIp ? ipIdeaItem.ideaUsages.length : ipIdeaItem?.sysIps.length }}</a-tag>
+        <a-tag>总条数：{{ ipIdeaItem?.showIp ? ipIdeaItem.ideaUsages.length : ipIdeaItem?.sysIps.length }} </a-tag>
         <a-typography-text v-if="ipIdeaItem?.identity.length > 0" copyable>
           <span>{{ ipIdeaItem.identity }}</span>
           <!--        <span :class="item.status === -1 ? 'line-through text-gray-400' : ''">{{ item.identity }}</span>-->
@@ -100,6 +113,7 @@
   import CommonList from '/@/views/a/common/CommonList.vue';
   import { getList, saveOrUpdate } from '/@/views/a/pms/IdeaMember.api';
   import IdeaSysIpInfo from './IdeaSysIpInfo.vue';
+
   const advanced = ref(false);
 
   const showDrawer = ref(false);
@@ -145,7 +159,27 @@
     emit('handleEdit', record);
   };
   const CommonListRef = ref();
-
+  const getLocation = (item) => {
+    if (item?.sysIps?.length > 0) {
+      let location = item?.sysIps[0];
+      return (
+        (location?.country?.indexOf('中国') > -1 || location?.country?.toUpperCase()?.indexOf('CHINA') > -1 ? '' : location?.country) +
+        location.province +
+        location.city +
+        location.county +
+        ' | ' +
+        location.browser
+      );
+    }
+    return '';
+  };
+  const getDevice = (item) => {
+    if (item?.sysIps?.length > 0) {
+      let device = item?.sysIps[0];
+      return device.operator + ' | ' + device.model + ' | ' + device.system;
+    }
+    return '';
+  };
   defineExpose({
     startQuery: (params = {}) => {
       CommonListRef.value.execQuery(getList, params);
